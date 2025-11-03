911955db051f4b3463f8b0f5a83c3dbc164b4b76
sik2 <bickwang123@gmail.com>
2025-11-03 21:11:53 +0900

    main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,36 @@
-import { data } from "../../dummyData/dummy";
+import { useEffect, useState } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?language=ko-KR&api_key=ac770053b0b8a5509c03c3d6e2110597"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setMovies(res.results);
+      });
+  }, []);
+
   return (
     <>
-      <h1>메인 페이지</h1>
-      {data.results.map((row) => (
-        <li>{row.title}</li>
-      ))}
+      <Carousel
+        showThumbs
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        transitionTime={3}
+      >
+        {movies.map((movie) => (
+          <img
+            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+            alt="영화 포스터"
+          />
+        ))}
+      </Carousel>
     </>
   );
 }
