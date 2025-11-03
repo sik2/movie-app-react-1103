450fb3d453ddc839001180f27a0b3d3b98fe8d32
sik2 <bickwang123@gmail.com>
2025-11-03 20:58:40 +0900

    header 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Moive from "./pages/Movie";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route index element={<Home />}></Route>
         <Route path="movie" element={<Moive />}></Route>
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,10 +1,8 @@
 import { data } from "../../dummyData/dummy";
-import Header from "../components/Header";
 
 function Moive() {
   return (
     <>
-      <Header />
       <h1>서브 페이지</h1>
       {data.results.map((row) => (
         <li>{row.title}</li>
