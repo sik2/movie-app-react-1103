a0cc3cff07541145b002e66234ff8b621e844547
sik2 <bickwang123@gmail.com>
2025-11-10 21:17:48 +0900

    searchbox

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,5 +1,23 @@
-function SearchBox() {
-  return <>검색창</>;
+function SearchBox({ keyword, onChangeKeyword }) {
+  return (
+    <div className="w-full mt-2.5">
+      <div className="relative flex w-full max-w-[60%] mx-auto items-center gap-2 bg-white rounded-full shadow-sm">
+        {/* 검색 아이콘 */}
+        <div className="absolute top-1/2 left-3 -translate-y-1/2 flex items-center">
+          🔍
+        </div>
+
+        {/* 입력창 */}
+        <input
+          type="text"
+          placeholder="영화 제목을 입력하세요"
+          value={keyword}
+          onChange={onChangeKeyword}
+          className="w-full block py-2 px-5 pl-9 border-0 outline-none rounded-full text-gray-800"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default SearchBox;
